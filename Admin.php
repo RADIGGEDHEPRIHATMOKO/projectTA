8ef1e79dd1e17a3d0e2f5a6433b6c9b21bd9a136
RADIGGEDHEPRIHATMOKO <90669051+RADIGGEDHEPRIHATMOKO@users.noreply.github.com>
2023-08-30 10:54:08 +0700

    Add files via upload

---
+++ Admin.php
@@ -0,0 +1,37 @@
+<?php
+
+class Admin Extends CI_Controller{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('admin/Admin_model');
+    }
+
+    public function index()
+    {
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email', 
+            array(
+             'required' => 'tidak boleh kosong',
+             'valid_email' => 'Email tidak valid "@"'
+            )    
+        );
+
+        $this->form_validation->set_rules('password', 'Password', 'required', 
+            array(
+             'required' => 'tidak boleh kosong'
+            )    
+        );
+
+        if($this->form_validation->run() == FALSE){
+            $this->load->view('admin/form_login');
+        }else{
+            $email = $this->input->post('email');
+            $password = $this->input->post('password');
+            $this->Admin_model->prosesLogin($email,$password);
+        }
+
+        
+    }
+}
